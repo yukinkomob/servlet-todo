e01fb4154dd3c540292e93a8241298ba3e8e15c5
Nishimura <yukinkopen@gmail.com>
2021-07-01 05:04:58 +0900

    [mod] サーブレットの名称を変更

--- src/com/sample/TaskServlet.java
+++ src/com/sample/TaskServlet.java
@@ -1,8 +1,6 @@
 package com.sample;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class SampleServlet
+ * Servlet implementation class TaskServlet
  */
-@WebServlet("/sample")
-public class SampleServlet extends HttpServlet {
+@WebServlet("/task")
+public class TaskServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public SampleServlet() {
+    public TaskServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -28,16 +26,16 @@ public class SampleServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		out.println("<html><body><h1>hello.</h1></body></html>");
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
 	}
 
 }
